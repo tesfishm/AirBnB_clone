051d5ded65ad98611768fd27a0e88c54d8af23de
ephayeabe <ephremyeabe@gmail.com>
2022-01-26 18:10:12 +0300

    Update your command interpreter (console.py) to allow those actions: show, create, destroy, update and all with all classes created previously.

--- console.py
+++ console.py
@@ -4,10 +4,18 @@
 import cmd
 from datetime import datetime
 import models
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.user import User
+import shlex  # for splitting the line along spaces except in double quotes
 
-classes = {"BaseModel": BaseModel, "User": User}
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
 
 class HBNBCommand(cmd.Cmd):
     """ HBNH console """
