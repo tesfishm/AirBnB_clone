cbe70b5a5ea1b8ddc3f64c1829bd633c27d69735
ephayeabe <ephremyeabe@gmail.com>
2022-01-26 17:43:57 +0300

    update console.py

--- console.py
+++ console.py
@@ -8,4 +8,16 @@ from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
-    pass
+    """ HBNH console """
+    prompt = '(hbnb) '
+
+    def do_EOF(self, arg):
+        """Exits console"""
+        return True
+		
+	def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
